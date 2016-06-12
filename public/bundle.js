ada430634d18a4f9fb07abb1d421e06199e63fc4
Willian Justen <willianjustenqui@gmail.com>
2016-06-12 19:03:16 -0300

    Add repo and bio

--- public/bundle.js
+++ public/bundle.js
@@ -25126,26 +25126,53 @@
 /* 221 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(1);
 
 	var Repos = React.createClass({
-	    displayName: 'Repos',
+	    displayName: "Repos",
 
 	    propTypes: {
 	        username: React.PropTypes.string.isRequired,
 	        repos: React.PropTypes.array.isRequired
 	    },
 	    render: function render() {
-	        console.log('Repos', this.props.repos);
+	        console.log(this.props.repos);
+	        var repos = this.props.repos.map(function (repo, index) {
+	            return React.createElement(
+	                "li",
+	                { className: "list-group-item", key: index },
+	                repo.html_url && React.createElement(
+	                    "h4",
+	                    null,
+	                    React.createElement(
+	                        "a",
+	                        { href: repo.html_url },
+	                        repo.name
+	                    )
+	                ),
+	                repo.description && React.createElement(
+	                    "p",
+	                    null,
+	                    " ",
+	                    repo.description,
+	                    " "
+	                )
+	            );
+	        });
 	        return React.createElement(
-	            'div',
+	            "div",
 	            null,
 	            React.createElement(
-	                'p',
+	                "h3",
 	                null,
-	                'REPOS'
+	                "User Repos"
+	            ),
+	            React.createElement(
+	                "ul",
+	                { className: "list-group" },
+	                repos
 	            )
 	        );
 	    }
@@ -25157,33 +25184,85 @@
 /* 222 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(1);
 
 	var UserProfile = React.createClass({
-	    displayName: 'UserProfile',
+	    displayName: "UserProfile",
 
 	    propTypes: {
 	        username: React.PropTypes.string.isRequired,
 	        bio: React.PropTypes.object.isRequired
 	    },
 	    render: function render() {
-	        console.log('Bio', this.props.bio);
 	        return React.createElement(
-	            'div',
+	            "div",
 	            null,
-	            React.createElement(
-	                'p',
-	                null,
-	                'UserProfile '
+	            this.props.bio.avatar_url && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                " ",
+	                React.createElement("img", { src: this.props.bio.avatar_url, className: "img-rounded img-responsive" })
 	            ),
-	            React.createElement(
-	                'p',
-	                null,
-	                'Username: ',
-	                this.props.username,
-	                ' '
+	            this.props.bio.name && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Name: ",
+	                this.props.bio.name
+	            ),
+	            this.props.bio.login && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Username: ",
+	                this.props.bio.login
+	            ),
+	            this.props.bio.email && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Email: ",
+	                this.props.bio.email
+	            ),
+	            this.props.bio.location && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Location: ",
+	                this.props.bio.location
+	            ),
+	            this.props.bio.company && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Company: ",
+	                this.props.bio.company
+	            ),
+	            this.props.bio.followers && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Followers: ",
+	                this.props.bio.followers
+	            ),
+	            this.props.bio.following && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Following: ",
+	                this.props.bio.following
+	            ),
+	            this.props.bio.following && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Public Repos: ",
+	                this.props.bio.public_repos
+	            ),
+	            this.props.bio.blog && React.createElement(
+	                "li",
+	                { className: "list-group-item" },
+	                "Blog: ",
+	                React.createElement(
+	                    "a",
+	                    { href: this.props.bio.blog },
+	                    " ",
+	                    this.props.bio.blog
+	                )
 	            )
 	        );
 	    }
